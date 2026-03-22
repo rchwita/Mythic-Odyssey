62dded5288b7ee183a0f6e71b98cb7dffe90fd23
Md. Ridowan Ahmed <ridowan.ahmed763@gmail.com>
2026-03-22 12:44:58 +0600

    how to play page added

--- scripts/source_scripts/main_menu.gd
+++ scripts/source_scripts/main_menu.gd
@@ -3,5 +3,8 @@ extends Control
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/UI/level_menu.tscn")
 
+func _on_how_to_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/how_to_play.tscn")
+
 func _on_exit_pressed() -> void:
 	get_tree().quit()
